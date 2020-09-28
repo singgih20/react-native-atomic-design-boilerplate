1b2c9814fa76ea12417cdc7ee2e5183810dbc9f4
Singgih <gokonggokong71@gmail.com>
2020-09-28 09:15:26 +0700

    add utils/colors

---
+++ src/utils/colors/index.js
@@ -0,0 +1,27 @@
+const mainColors = {
+    green1: '#0BCAD4',
+    dark1: '#112340',
+    grey1: '#7D8797'
+}
+
+export const color = {
+    primary: mainColors.green1,
+    secondary: mainColors.dark1,
+    white: 'white',
+    black: 'black',
+    text: {
+        default: mainColors.dark1,
+        secondary: mainColors.grey1
+    },
+    button: {
+        primary: {
+            background: colors.primary,
+            text: colors.white
+        },
+        secondary: {
+            background: colors.white,
+            text: colors.black
+        }
+    }
+}
+
